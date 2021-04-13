372512731de4f25fdbc5bf31ba6e72de589a9f80
Jean-Michel Hautbois <jeanmichel.hautbois@ideasonboard.com>
2021-04-13 07:56:51 +0200

    ipa: ipu3: Add support for IPU3 AEC/AGC algorithm
    Inherit from the Algorithm class to implement basic auto-exposure and
    auto-gain functions.
    Extract computeTargetExposure() and computeGain() and adapt those to the
    IPU3 structure.
    Filtering was added too as it avoids big steps when exposure changes a
    lot.
    
    Signed-off-by: Jean-Michel Hautbois <jeanmichel.hautbois@ideasonboard.com>

--- src/ipa/ipu3/ipu3.cpp
+++ src/ipa/ipu3/ipu3.cpp
@@ -21,6 +21,7 @@
 #include "libcamera/internal/buffer.h"
 #include "libcamera/internal/log.h"
 
+#include "ipu3_agc.h"
 #include "ipu3_awb.h"
 
 static constexpr uint32_t kMaxCellWidthPerSet = 160;
@@ -70,6 +71,8 @@ private:
 
 	/* Interface to the AWB algorithm */
 	std::unique_ptr<ipa::IPU3Awb> awbAlgo_;
+	/* Interface to the AEC/AGC algorithm */
+	std::unique_ptr<ipa::IPU3Agc> agcAlgo_;
 
 	/* Local parameter storage */
 	struct ipu3_uapi_params params_;
@@ -168,6 +171,9 @@ void IPAIPU3::configure(const std::map<uint32_t, ControlInfoMap> &entityControls
 
 	awbAlgo_ = std::make_unique<ipa::IPU3Awb>();
 	awbAlgo_->initialise(params_, bdsOutputSize, bdsGrid_);
+
+	agcAlgo_ = std::make_unique<ipa::IPU3Agc>();
+	agcAlgo_->initialise(bdsGrid_);
 }
 
 void IPAIPU3::mapBuffers(const std::vector<IPABuffer> &buffers)
@@ -239,7 +245,8 @@ void IPAIPU3::processControls([[maybe_unused]] unsigned int frame,
 
 void IPAIPU3::fillParams(unsigned int frame, ipu3_uapi_params *params)
 {
-	awbAlgo_->updateWbParameters(params_, 1.0);
+	if (agcAlgo_->updateControls())
+		awbAlgo_->updateWbParameters(params_, agcAlgo_->gamma());
 
 	*params = params_;
 
@@ -254,7 +261,10 @@ void IPAIPU3::parseStatistics(unsigned int frame,
 {
 	ControlList ctrls(controls::controls);
 
+	agcAlgo_->process(stats, exposure_, gain_);
 	awbAlgo_->calculateWBGains(stats);
+	if (agcAlgo_->updateControls())
+		setControls(frame);
 
 	ipa::ipu3::IPU3Action op;
 	op.op = ipa::ipu3::ActionMetadataReady;
